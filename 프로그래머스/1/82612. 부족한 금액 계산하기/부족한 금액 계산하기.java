5e22d0568ebdae63a47060c4693e87fcd240e24e
Lustella-123 <tella4164@gmail.com>
2024-11-10 13:39:07 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.12 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -1,15 +1,9 @@
 class Solution {
     public long solution(int price, int money, int count) {
-        long result = 0;
         long usedMoney = 0;
         for (int i = 1; i <= count; i++) {
             usedMoney += (long)price * i;
         }
-        if (money >= usedMoney) {
-            result = 0;
-        } else {
-            result = usedMoney - money;
-        }
-        return result;  
+        return money >= usedMoney ? 0 : usedMoney - money;
     }
 }
