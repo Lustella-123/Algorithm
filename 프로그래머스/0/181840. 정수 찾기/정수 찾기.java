fcf59c998f9c5347f1442a59ef5e5ce8662d3db8
Lustella-123 <tella4164@gmail.com>
2025-04-01 14:08:58 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        for(int i = 0; i < num_list.length; i++) {
-            if(n==num_list[i]) {
+        for(int num : num_list) {
+            if(n==num) {
                 return 1;
             }
         }
